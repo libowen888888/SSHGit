1bc6692b73db9d676b76f8ba2b850677ffaab32a
libowen <37988888@qq.com>
2019-11-20 11:55:48 +0800

    第二次提交

---
+++ src/main/java/cn/ssh/test/TT.java
@@ -0,0 +1,8 @@
+package cn.ssh.test;
+
+public class TT {
+
+    public static void main(String[] args) {
+        System.out.println("----------------");
+    }
+}
--- src/main/java/cn/ssh/test/TestGit.java
+++ src/main/java/cn/ssh/test/TestGit.java
@@ -3,6 +3,8 @@ package cn.ssh.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("-------00000000000-----------");
+
+        System.out.println("000000000000000");
     }
 
 }
